543a2252c9d52b29e3fd759fecb4643e30211fe7
wambui7 <shiko.sheila@gmail.com>
2018-08-11 10:59:40 -0500

    Add project files.

---
+++ maths application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace maths_application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
